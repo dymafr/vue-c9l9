5d72e84169dc9f66929d864600dd3623fabd3cef
dymafr <erwan1309@hotmail.com>
2022-03-09 14:17:03 +0100

    1 file modified, 2 added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <button @click.once="display = true">display</button>
   <div v-if="display">
-    <Blog />
+    <LazyBlog />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import Blog from './Blog.vue';
+import { ref, defineAsyncComponent } from 'vue';
+
+const LazyBlog = defineAsyncComponent({
+  loader: () =>
+    new Promise<any>((resolve) => {
+      setTimeout(() => {
+        import('./Blog.vue').then((c) => resolve(c));
+      }, 3000);
+    }),
+  loadingComponent: Loader,
+  delay: 200,
+  errorComponent: Error,
+  timeout: 2000,
+});
 
 const display = ref(false);
 </script>
---
+++ src/Error.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Oh no...</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
