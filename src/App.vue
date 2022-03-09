cf11659ff21bf0b26245f2af9f4a4a4c13a5eeff
dymafr <erwan1309@hotmail.com>
2022-03-09 14:18:41 +0100

    1 file modified, 2 added, 2 deleted

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
 <script setup lang="ts">
 import { ref, defineAsyncComponent } from 'vue';
+import AppError from './AppError.vue';
+import AppLoader from './AppLoader.vue';
 
 const LazyBlog = defineAsyncComponent({
   loader: () =>
@@ -15,9 +17,9 @@ const LazyBlog = defineAsyncComponent({
         import('./Blog.vue').then((c) => resolve(c));
       }, 3000);
     }),
-  loadingComponent: Loader,
+  loadingComponent: AppLoader,
   delay: 200,
-  errorComponent: Error,
+  errorComponent: AppError,
   timeout: 2000,
 });
 
